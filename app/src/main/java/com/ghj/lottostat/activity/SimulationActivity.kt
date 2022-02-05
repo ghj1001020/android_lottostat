015d09530c076f2544ca09482fc6a9c72aa29ec7
ghj1001020 <33592073+ghj1001020@users.noreply.github.com>
2022-02-05 14:50:45 +0900

    시뮬레이션

--- app/src/main/java/com/ghj/lottostat/activity/SimulationActivity.kt
+++ app/src/main/java/com/ghj/lottostat/activity/SimulationActivity.kt
@@ -1,11 +1,13 @@
 package com.ghj.lottostat.activity
 
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import com.ghj.lottostat.R
 import com.ghj.lottostat.activity.base.BaseDrawerViewModelActivity
 import com.ghj.lottostat.activity.viewmodel.SimulationViewModel
 import com.ghj.lottostat.databinding.ActivitySimulationBinding
 
-class SimulationActivity : BaseDrawerViewModelActivity<ActivitySimulationBinding, SimulationViewModel>() {
+class SimulationActivity : BaseDrawerViewModelActivity<ActivitySimulationBinding, SimulationViewModel>(), View.OnClickListener {
 
     override fun newContentBinding(): ActivitySimulationBinding {
         return ActivitySimulationBinding.inflate(layoutInflater)
@@ -16,7 +18,20 @@ class SimulationActivity : BaseDrawerViewModelActivity<ActivitySimulationBinding
     }
 
     override fun onCreateAfter() {
-
+        initLayout()
     }
 
+    fun initLayout() {
+        mContent.noContent.txtDesc.text = getString(R.string.no_number_simulation)
+        mContent.layoutTitle.setOnClickListener { v ->
+            mContent.chkFold.isChecked = !mContent.chkFold.isChecked
+            mContent.layoutResult.visibility = if( mContent.chkFold.isChecked ) View.VISIBLE else View.GONE
+        }
+        mContent.btnFilter.setOnClickListener(this)
+        mContent.btnSimulation.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+
+    }
 }
